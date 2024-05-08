6e1a4750b00cad3366fbfa1fdc944f39c9bcd648
swadeshchisim <schisim195@gmail.com>
2024-05-08 23:56:40 +0600

    initial functionality added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,3 +1,25 @@
+// function play() {
+//    //hide the home screen, to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//    //show the playground
+//    const playGroundSection = document.getElementById("play-ground");
+//    playGroundSection.classList.remove('hidden');
+// }
+
+function continueGame() {
+    // step - 1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly alphabet to the screen
+    const alphabetArea = document.getElementById('alphabet-area');
+    alphabetArea.innerText = alphabet;
+}
+
 function play() {
-    console.log('play now');
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
---
+++ script/utility.js
@@ -0,0 +1,33 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function getRandomAlphabet() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwxys';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+// function getRandomAlphabet() {
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+    
+//     //get a random index between 0 - 25
+//     const randomNumber = Math.random(alphabets) * 25;
+//     const index = Math.round(randomNumber);
+    
+//     const alphabet = alphabets[index];
+//     return alphabet;
+// }
