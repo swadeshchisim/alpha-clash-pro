3d902fd0d6e9de09506926f56dde4f3457675cd6
swadeshchisim <schisim195@gmail.com>
2024-05-10 17:19:30 +0600

    life-score is added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,12 +1,33 @@
-// function play() {
-//    //hide the home screen, to hide the screen add the class hidden to the home section
-//     const homeSection = document.getElementById('home-screen');
-//     homeSection.classList.add('hidden');
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
 
-//    //show the playground
-//    const playGroundSection = document.getElementById("play-ground");
-//    playGroundSection.classList.remove('hidden');
-// }
+    //key player is expected to press
+    const currentAlaphabetElement = document.getElementById('current-alphabet');
+    const currentAlaphabet = currentAlaphabetElement.innerText;
+    const expectedAlphabet = currentAlaphabet.toLowerCase();
+
+    //check right or wrong answer
+    if(playerPressed === expectedAlphabet) {
+        console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        console.log(currentScore);
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+        console.log(updatedScore);
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+
+    } else {
+        console.log('Wrong key pressed');
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame() {
     // step - 1: generate a random alphabet
@@ -14,7 +35,7 @@ function continueGame() {
     console.log('your random alphabet', alphabet);
 
     // set randomly alphabet to the screen
-    const alphabetArea = document.getElementById('alphabet-area');
+    const alphabetArea = document.getElementById('current-alphabet');
     alphabetArea.innerText = alphabet;
 
     // set background color
@@ -24,5 +45,5 @@ function continueGame() {
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
-    continueGame()
+    continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -1,16 +1,46 @@
+//hide element by id
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
+// show element by id
 function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
+// set background color by id
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
+}
+
+// set background color by id
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getTextElementById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.value;
+    console.log(elementValueText);
+    const value = parseFloat(elementValueText);
+    return value;
+}
+
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseFloat(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
 
 function getRandomAlphabet() {
@@ -23,16 +53,3 @@ function getRandomAlphabet() {
     const alphabet = alphabets[index];
     return alphabet;
 }
-
-// function getRandomAlphabet() {
-//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-//     const alphabets = alphabetString.split('');
-//     // console.log(alphabets);
-    
-//     //get a random index between 0 - 25
-//     const randomNumber = Math.random(alphabets) * 25;
-//     const index = Math.round(randomNumber);
-    
-//     const alphabet = alphabets[index];
-//     return alphabet;
-// }
